d997634a8fc5b25291639930cd315732b1b91402
Gun-Tanawat <tanay571@epicc.postbox.in.th>
2023-01-19 11:50:31 +0700

    Create user.js

---
+++ model/user.js
@@ -0,0 +1,9 @@
+// const mongoose = require("mongoose");
+
+// const userSchema = new mongoose.Schema({
+//   first_name: { type: String, default: null },
+//   phone: { type: String },
+//   token: { type: String },
+// });
+
+// module.exports = mongoose.model("user", userSchema);
